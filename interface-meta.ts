d38ed6827961bd380da747f70cd22f67febe452a
OpportunityLiu <opportunity@live.in>
2023-07-14 09:48:53 +0800

    remove

--- interface-meta.ts
+++
@@ -1,5 +0,0 @@
-import type { Foo } from "./foo-bar.js";
-
-export class B {
-  constructor(readonly foo: Foo) {}
-}
--- no-metadata.ts
+++
@@ -1,5 +0,0 @@
-import type { Bar } from "./foo-bar.js";
-
-export class B {
-  constructor(readonly bar: Bar) {}
-}
